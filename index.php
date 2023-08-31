8e303a92abfdb454244518a8cf3f8c7545e7eebc
Czeriss <cheroke1112@gmail.com>
2023-08-31 11:06:51 +0200

    add header and footer

--- index.php
+++ index.php
@@ -4,8 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Book Shop</title>
+    <link rel="stylesheet"  href="style.css">
 </head>
 <body>
-    
+    <header>
+        <img  src="image/logo.png" alt="There should be my logo" class="logo" title="This is my logo">
+
+        <hr>
+    </header>
+
+    <footer>
+        <!-- I can add my email  -->
+        <span class="text-left">Author: Cheriss</span><a class="text-right" href="mailto:myMail@mail.com">Mail to me!</a>
+    </footer>
 </body>
 </html>
