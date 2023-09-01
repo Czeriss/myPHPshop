4684eb95ee97cb01f913944d23e3e1c84f5308ab
Czeriss <cheroke1112@gmail.com>
2023-09-01 15:10:43 +0200

    add new file

---
+++ checkout.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Checkout</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <?php
+        include("header.html");
+    ?>
+    <div>
+
+
+    </div>
+    <?php
+        include("footer.html");
+    ?>
+</body>
+</html>
